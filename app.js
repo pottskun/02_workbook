9eae8abaf79625ccbe4c633640e5a103116a3551
potttskun <wso.aphwx_vs.9@icloud.com>
2025-04-06 16:27:52 +0900

    ブックマーク機能完成

--- app.js
+++ app.js
@@ -1,19 +1,14 @@
 fetch('data.json')
   .then(response => response.json())  // JSONデータをオブジェクトに変換
   .then(data => { //jsonのデータはdataに入っている
-
     const container = document.getElementById('js-question-list');
-    // HTMLにある'js-question-list'というIDの要素をcontainerという変数に格納する
-    // 最終的にはcontainerの中に子要素としてhogeが追加されるようになっている
-    // hogeには生成したliが入っている、クラスも追加している(question-item)
-
-    // ここではtrueであれば問題が表示されるxxx
     if (data && data.beginnerProblems && data.beginnerProblems.length > 0) {
+      //data（JSONから取得したオブジェクト）が存在するか、data.beginnerProblemsが存在していて、さらにその配列が空でないことを確認、この条件が満たされると、実際に問題をリストとして表示する処理が走る
       // if (data && data.intermediateProblems && data.intermediateProblems.length > 0) {
       // if (data && data.AdvancedProblems && data.AdvancedProblems.length > 0) {
       // if (data && data.AdvancedProblems && data.AdvancedProblems.length > 0) {
-      // このif文はどういう意味？todo
-      const bookmarkedStates = {};
+      let bookmarkedStates = JSON.parse(localStorage.getItem("bookmarkedStates")) || {};
+      // ローカルストレージに"bookmarkedStates"があればそれをオブジェクト形式で取得、なければ空のオブジェクト {} を代入
       data.beginnerProblems.forEach(function (problem) {
         const questionItem = document.createElement("li");
         const question = document.createElement("div");
@@ -33,6 +28,7 @@ fetch('data.json')
 
         // アイコンを生成
         const bkmIcon = document.createElement("i");
+        // pタグとかでなくiタグ？？？xx
         bkmIcon.classList.add('bkm-icon', 'far', 'fa-thin', 'fa-bookmark', 'fa-xl');
         // bkmIcon.setAttribute('aria-hidden', 'true');xxx
         bkmBtn.appendChild(bkmIcon);
@@ -84,11 +80,18 @@ fetch('data.json')
             bkmIcon.classList.add('far');
           }
         });
-        // bkmbtnをクリックしたときにそのidをローカルストレージに保存する
         bkmBtn.addEventListener('click', function () {
-          let bookmarkedStates = JSON.parse(localStorage.getItem("bookmarkedStates")) || {};
-          bookmarkedStates[problem.id] = true;
-          localStorage.setItem("bookmarkedStates", JSON.stringify(bookmarkedStates));
+          let bookmarkedStates = JSON.parse(localStorage.getItem("bookmarkedStates"))
+          // bkmbtnをクリックしたときにそのidをローカルストレージに保存する
+          if (bkmIcon.classList.contains('fa')) {
+            bookmarkedStates[problem.id] = true;
+            // クリックされた問題のidをキーとして、bookmarkedStatesのオブジェクト(ローカルストレージ)にtrueを設定する？保存する？
+          } else {
+            // bookmarkedStates に何かデータが存在していれば true になり、中の処理が実行される？
+            delete bookmarkedStates[problem.id];
+          }
+          localStorage.setItem('bookmarkedStates', JSON.stringify(bookmarkedStates));
+          // json形式の文字列に変えて更新されたデータを保存
         });
       })
       localStorage.setItem("questionID", JSON.stringify(data.beginnerProblems));
@@ -102,9 +105,7 @@ fetch('data.json')
   });
 
 
-// 1.クリックされた問題の id を取得する。
-// 2.bookmarkedStates オブジェクトで、問題の id に対応するブクマ状態を取得する。
-// 3.ブクマ状態を反転させる（true なら false、false なら true にする）。
-// 4.bookmarkedStates オブジェクトのブクマ状態を更新する。
-// 5.localStorage にブクマ情報を保存する。
-// 6.ブクマボタンの表示を更新する。
+  // 「既存のデータを取得してから操作する処理」を追加する
+  // 最初にローカルストレージから既存のデータを取得する
+  // 今回クリックしたデータを既存のデータと結合する（追加 or 削除）
+  // その結果を改めてローカルストレージに保存する
